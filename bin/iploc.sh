c90011b1a069cf82cae18e92ed67817510196c44
bohoomil <bohoomil@gmail.com>
2012-01-18 06:16:42 +0100

    cleaning up

--- bin/iploc.sh
+++ bin/iploc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-myip=`dig myip.opendns.com @resolver1.opendns.com +short`
-loc=`geoiplookup $myip | cut -c 24-25`
+myip=$(dig myip.opendns.com @resolver1.opendns.com +short)
+loc=$(geoiplookup $myip | awk -F' ' '{print $4}' | sed '$s/,$//')
 
-echo "IP:$myip"; 
+echo "IP: $myip"; 
 
-echo -en "country: \e[01;35m $loc\n";
+echo -en "country:\e[01;35m $loc\e[0m\n";
 
 exit 0
--- bin/loc.sh
+++ bin/loc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# returns external IP's country code 
 
-myip=`dig myip.opendns.com @resolver1.opendns.com +short`
-loc=`geoiplookup $myip | cut -c 24-25`
+myip=$(dig myip.opendns.com @resolver1.opendns.com +short)
+loc=$(geoiplookup $myip | awk -F' ' '{print $4}' | sed '$s/,$//')
 
 echo "$loc"
 
